f96cb111e2d9f48c7c5001ca9de9df5b00054258
hacker123Coder <akshaykd117@gmail.com>
2022-05-13 01:04:41 +0530

    new commit1

---
+++ 22_conditional.py
@@ -0,0 +1,17 @@
+a = 55
+
+#1
+# if(a > 4):
+#     print("a is greater than 4")    # here both statements are true but it will print only
+# elif(a > 5):                        # the first statement
+#     print("a is greater than 5")    
+# else:
+#     print("a is smaller than 4")
+
+#2
+if(a > 4):
+    print("a is greater than 4")
+if(a > 5):
+    print("a is greater than 5")  #here the 4 spaces before print is called as "indention"
+else:
+    print("a is not greater than 4 or 5")
---
+++ 23_quiz_problem.py
@@ -0,0 +1,14 @@
+userAge= int(input("enter your age:"))
+
+# Method 1
+# if( userAge>=18):
+#     print("yes")
+# else:
+#     print("no")
+
+# Method 2
+
+if( userAge>1 and  userAge<4):
+    print("you are a baby")
+else:
+    print("no you'r not a baby anymore")
